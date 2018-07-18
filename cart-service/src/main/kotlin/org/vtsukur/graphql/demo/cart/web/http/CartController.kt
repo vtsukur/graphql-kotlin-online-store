73d7e0c95e9cd72081657fe6de4427ad1c01e15f
vtsukur <volodymyrt@wix.com>
2018-07-18 16:11:47 +0300

    Fixing GET for `/carts/{id}`

--- cart-service/src/main/kotlin/org/vtsukur/graphql/demo/cart/web/http/CartController.kt
+++ cart-service/src/main/kotlin/org/vtsukur/graphql/demo/cart/web/http/CartController.kt
@@ -13,7 +13,7 @@ class CartController(private val cartService: CartService, private val productSe
     @RequestMapping("/carts/{id}")
     @ResponseBody
     operator fun get(@PathVariable id: Long,
-                     @RequestParam(value = "projection", required = false) projection: String): Any? {
+                     @RequestParam(value = "projection", required = false) projection: String?): Any? {
         val cart = cartService.findCart(id)!!
         return if ("with-products" == projection) {
             getProjectionWithProducts(cart)
